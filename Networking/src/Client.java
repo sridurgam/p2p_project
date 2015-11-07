44748a43793e10b52ca32051aba6848e0cf8b646
sridurgam <srivatsavad@gmail.com>
2015-11-06 21:44:35 -0500

    Added the File Transfer feature

--- Networking/src/Client.java
+++ Networking/src/Client.java
@@ -1,27 +1,58 @@
 import java.io.*;
 import java.net.*;
-import java.util.*;
 
 public class Client {
+	public final static int FILE_SIZE = 9322386;
+	
 	public void createPeers() throws UnknownHostException, IOException{
-		Scanner inputScanner = new Scanner(System.in);
-		Socket clientSocket = new Socket("127.0.0.1", 8081);		
-		Scanner socketScanner = new Scanner(clientSocket.getInputStream());		
-		//System.out.println("Enter any number:");
-		int number = 22;		
-		PrintStream p = new PrintStream(clientSocket.getOutputStream());
-		p.println(number);		
-		int number1 = socketScanner.nextInt();
-		System.out.print(number1+"\n");
+		int bytesRead;
+		int current = 0;
 		
-		inputScanner.close();
-		clientSocket.close();
-		socketScanner.close();
+		FileOutputStream fOutStream = null;
+		BufferedOutputStream bOutStream = null;
+		FileInputStream inStream = null;
+		Socket socket = null;
+		
+		try{
+			socket = new Socket("127.0.0.1", 8081);
+			System.out.println("Connected to server!");
+			
+			byte [] byteArray = new byte [FILE_SIZE];
+			inStream = (FileInputStream)socket.getInputStream();
+			
+			fOutStream = new FileOutputStream("TransferDoc.pdf");
+			bOutStream = new BufferedOutputStream(fOutStream);
+			bytesRead = inStream.read(byteArray, 0, byteArray.length);
+			current = bytesRead;
+			
+			do{
+				bytesRead = inStream.read(byteArray, 0, byteArray.length);
+				if (bytesRead >= 0){
+					current += bytesRead;
+				}
+			}while(bytesRead > -1);
+			bOutStream.write(byteArray, 0, current);
+			bOutStream.flush();
+			
+			System.out.println("File transfer complete!");
+			
+		}finally{
+			if (bOutStream != null){
+				bOutStream.close();
+			}
+			if (fOutStream != null){
+				fOutStream.close();
+			}
+			if (socket != null){
+				socket.close();
+			}
+		}
 	}
 	public static void main(String args[]) throws UnknownHostException, IOException{
-	Client peer1 = new Client();
-	peer1.createPeers();
-	Client peer2 = new Client();
-	peer2.createPeers();
+		Client peer1 = new Client();
+		peer1.createPeers();
+		
+		Client peer2 = new Client();
+		peer2.createPeers();
 	}
 }
