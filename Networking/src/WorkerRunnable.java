359b36ad4e640f45fc955726df2ba3caff972e4c
sridurgam <srivatsavad@gmail.com>
2015-11-06 21:42:54 -0500

    Added the File Transfer feature

--- Networking/src/WorkerRunnable.java
+++ Networking/src/WorkerRunnable.java
@@ -1,29 +1,55 @@
-import java.io.PrintStream;
-import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class WorkerRunnable implements Runnable{
 
     protected Socket serverSocket = null;
+    protected FileInputStream fInStream  = null;
+	protected BufferedInputStream bInStream = null;
+	protected FileOutputStream outStream = null;
+	protected File file = null;
 
-    public WorkerRunnable(Socket serveSocket) {
-        this.serverSocket = serveSocket;
+	public WorkerRunnable(Socket serverSocket, int chunkNum) {
+        this.serverSocket = serverSocket;
+        this.file = new File(System.getProperty("user.dir") + "\\src\\TransferDoc.pdf");
+        System.out.println(this.file);
     }
 
     public void run() {
-        try {
-            Scanner socketScanner = new Scanner(serverSocket.getInputStream());
-    		int number = socketScanner.nextInt();
-    		number *= 2;
-    		PrintStream printStream = new PrintStream(serverSocket.getOutputStream());
-    		printStream.println(number);
-    		socketScanner.close();
-    		serverSocket.close();
-        } 
-        catch (IOException e) 
-        {
-            e.printStackTrace();
-        }
+        try{
+	    	try {
+	        	byte [] byteArray = new byte[(int)this.file.length()];
+				fInStream = new FileInputStream(this.file);
+				bInStream = new BufferedInputStream(fInStream);
+				bInStream.read(byteArray, 0, byteArray.length);
+				
+				outStream = (FileOutputStream)serverSocket.getOutputStream();
+				System.out.println("Sending file...");
+				outStream.write(byteArray, 0, byteArray.length);
+				outStream.flush();
+				
+				System.out.println("File transfer complete");
+				System.out.println("Transferred " + this.file.length() + " bytes");
+	        } finally {
+				if (bInStream!=null){
+					bInStream.close();
+				}
+				if (fInStream!=null){
+					fInStream.close();
+				}
+				if (outStream!=null){
+					outStream.close();
+				}
+				if (serverSocket != null){
+					serverSocket.close();
+				}
+			}
+        } catch (Exception e){
+			e.printStackTrace();
+		}
     }
 }
